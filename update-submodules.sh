ed8afb0fc0c85f7c920c9349096e6ee7dfa2ec8c
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2023-09-28 17:30:20 +0200

    feat(submodules): Added a script to automatically update submodules and push them to git

---
+++ update-submodules.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Initialiser tous les sous-modules
+git submodule init
+
+# Mettre à jour tous les sous-modules
+git submodule update --recursive --remote
+
+git add .
+
+# Facultatif : effectuer un commit pour enregistrer les mises à jour des sous-modules
+git commit -m "chore(submodules): Mise à jour des sous-modules."
+
+# Facultatif : pusher les modifications vers le dépôt parent
+git push
