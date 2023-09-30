6ff532d6c5d9dffd311527dbc043508278da762e
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2023-09-30 15:23:45 +0200

    fix(script): Fixed commit message of the submodule update script to english instead of french

--- update-submodules.sh
+++ update-submodules.sh
@@ -9,7 +9,7 @@ git submodule update --recursive --remote
 git add .
 
 # Facultatif : effectuer un commit pour enregistrer les mises à jour des sous-modules
-git commit -m "chore(submodules): Mise à jour des sous-modules."
+git commit -m "chore(submodules): Updating submodules."
 
 # Facultatif : pusher les modifications vers le dépôt parent
 git push
